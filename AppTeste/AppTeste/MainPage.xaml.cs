3d56cce69ec37730248dd921ba273df41a1bd832
N3rdy <n3rdydzn@hotmail.com>
2023-03-15 10:12:40 -0300

    Aula

---
+++ AppTeste/AppTeste/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTeste
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string nome = txt_nome.Text;
+
+            DisplayAlert("Olá", $"{nome}", "Ok");
+        }
+    }
+}
